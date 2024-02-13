cc2163f039ca03518ee9e4a5715d282f688ba0b8
KirjikTeremok <91903921+KirjikTeremok@users.noreply.github.com>
2024-02-13 19:56:02 +0300

    first commit for fun

---
+++ computerVisionLab1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace computerVisionLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxJ = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnGeneration = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(238, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(255, 400);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(23, 15);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(81, 30);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(509, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ввод данных";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(12, 123);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(56, 20);
+            this.textBoxX.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(15, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(90, 98);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Y";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(87, 123);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(56, 20);
+            this.textBoxY.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(163, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Яркость";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBoxJ
+            // 
+            this.textBoxJ.Location = new System.Drawing.Point(163, 123);
+            this.textBoxJ.Name = "textBoxJ";
+            this.textBoxJ.Size = new System.Drawing.Size(56, 20);
+            this.textBoxJ.TabIndex = 9;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(10, 159);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(204, 25);
+            this.btnDownload.TabIndex = 11;
+            this.btnDownload.Text = "загрузить данные";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnGeneration
+            // 
+            this.btnGeneration.Location = new System.Drawing.Point(10, 244);
+            this.btnGeneration.Name = "btnGeneration";
+            this.btnGeneration.Size = new System.Drawing.Size(112, 25);
+            this.btnGeneration.TabIndex = 12;
+            this.btnGeneration.Text = "генерация ";
+            this.btnGeneration.UseVisualStyleBackColor = true;
+            this.btnGeneration.Click += new System.EventHandler(this.btnGeneration_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(157, 246);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 448);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnGeneration);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxJ);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        private System.Windows.Forms.Button btnGeneration;
+
+        private System.Windows.Forms.Button btnDownload;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxJ;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.Button btnLoad;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        #endregion
+    }
+}
---
+++ computerVisionLab1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace computerVisionLab1
+{
+    public partial class Form1 : Form
+    {
+
+        private DataTable data;
+        private string selectedValue = "0";
+        public Form1()
+        {
+            InitializeComponent();    
+            for (int i = 1; i < 101; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+
+            comboBox1.SelectedItem = 1;
+        }
+        
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                data = openFile(openFileDialog.FileName);
+                dataGridView1.DataSource = data;
+
+                Bitmap bmp = new Bitmap(400, 400);
+                Graphics g = Graphics.FromImage(bmp);
+                CreateBitmapFromData(g, bmp);
+                //pictureBox1.Image = bmp;
+            }
+
+        }
+
+
+        private DataTable openFile(string filePath) 
+        {
+            DataTable dataTable = new DataTable();
+            
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string[] header = "X Y Яркость".Split(' ');
+                foreach (string column in header)
+                {
+                    dataTable.Columns.Add(column);
+                }
+
+                while (!reader.EndOfStream)
+                {
+                    string[] rows = reader.ReadLine().Split(' ');
+                    DataRow dataRow = dataTable.NewRow();
+                    for (int i = 0; i < rows.Length; i++)
+                    {
+                        dataRow[i] = rows[i];
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+
+            return dataTable;
+        }
+        
+        
+        
+        private void CreateBitmapFromData(Graphics g, Bitmap bitmap)
+        {
+            
+
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                for (int j = 0; j < data.Columns.Count; j++)
+                {
+                    
+                    FillNewPixel(g, data.Rows[i][0].ToString(), data.Rows[i][1].ToString(), data.Rows[i][2].ToString());
+                    
+                    
+                    //bitmap.SetPixel((int)(k*x), (int)(k*y), color);
+                }
+            }
+
+            pictureBox1.Image = bitmap;
+
+            // return bitmap;
+        }
+
+        private void FillNewPixel(Graphics g, string x, string y, string z)
+        {
+            float k = (float)400 / 1024;
+            Rectangle r = new Rectangle();
+            int X = int.Parse(x);
+            int Y = int.Parse(y);
+            int Z = int.Parse(z);
+            r.Width = 3;
+            r.Height = 3;
+            r.X = (int)(X * k);
+            r.Y = (int)(Y * k);
+            Color color = Color.FromArgb(Z, Z, Z);
+            Brush brush = new SolidBrush(color);
+            g.FillRectangle(brush, r);
+        }
+        
+        private void label4_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void btnGeneration_Click(object sender, EventArgs e) // добавляет в csv новые данные
+        {
+            Random rand = new Random();
+            string x, y, z;
+            int i;
+
+            for (i = 0; i < int.Parse(selectedValue); i++)
+            {
+                x = rand.Next(0, 1024).ToString();
+                y = rand.Next(0, 1024).ToString();
+                z = rand.Next(0, 255).ToString();
+                downloadPoint(x,y,z);
+            }
+            
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            downloadPoint(textBoxX.Text, textBoxY.Text, textBoxJ.Text);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var comboBox = (ComboBox)sender;
+            selectedValue = comboBox.SelectedItem.ToString();
+            
+        }
+
+
+        private void downloadPoint(string a, string b, string c)
+        {
+            StreamWriter sw = new StreamWriter("C:\\Users\\user\\Documents\\КириллТерещенко\\вуз\\шарпы\\ComputerVision\\data.csv", true, Encoding.ASCII);
+            if (a != null && b != null && c != null)
+            {
+                sw.WriteLine($"{a} {b} {c}");
+            }
+            sw.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}    
+
